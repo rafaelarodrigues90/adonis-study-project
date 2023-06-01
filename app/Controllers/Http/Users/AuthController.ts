62b34cc5bfb322294267b1150c1bb8aaff45eb59
Rafaela Rodrigues <ltcmdrdata@protonmail.com>
2023-06-01 18:43:15 -0300

    feat: 🎸 create routes register, login and logout

--- app/Controllers/Http/Users/AuthController.ts
+++ app/Controllers/Http/Users/AuthController.ts
@@ -16,4 +16,25 @@ export default class AuthController {
 
     return response.created(user)
   }
+
+  public async login({ request, response, auth }: HttpContextContract) {
+    const password = await request.input('password')
+    const email = await request.input('email')
+
+    try {
+      const token = await auth.use('api').attempt(email, password, {
+        expiresIn: '24hours',
+      })
+      return token.toJSON()
+    } catch (error) {
+      return response
+        .status(400)
+        .send({ error: { message: 'User with provided credentials could not be found' } })
+    }
+  }
+
+  public async logout({ auth, response }: HttpContextContract) {
+    await auth.logout()
+    return response.status(200)
+  }
 }
--- start/routes/users.ts
+++ start/routes/users.ts
@@ -2,4 +2,6 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
   Route.post('register', 'Users/AuthController.register').as('register')
+  Route.post('login', 'Users/AuthController.login').as('login')
+  Route.post('logout', 'Users/AuthController.logout').as('logout')
 }).prefix('api/v1/users/')
